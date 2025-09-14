446bb98f3280ef7ccdfbb7736bfddd15ccfe2713
uma <umamaheswari.g@everest.engineering>
2025-09-14 14:02:22 +0530

    Add productList component

---
+++ src/components/productList.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const ProductList = () => {
+  return (
+    <div className="product-list-container">
+      <h2>Products</h2>
+      
+    </div>
+  );
+};
+export default ProductList;
