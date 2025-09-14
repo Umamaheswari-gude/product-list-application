0d3b8cc405b938a0dd17f47e2f3ff6946b4a6032
uma <umamaheswari.g@everest.engineering>
2025-09-14 13:45:10 +0530

    Add ProductCard component

---
+++ src/components/productCard.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import { Product } from '../types/types'; 
+
+type ProductCardProps = {
+  productInfo: Product;
+}
+const ProductCard = ({ productInfo }: ProductCardProps) => {
+  return (
+    <div className="product-card">
+      
+    </div>
+  );
+};
+export default ProductCard;
